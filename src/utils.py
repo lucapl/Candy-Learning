ebaf4b997da652c8cd9f8700ab0f2123b1a6b2d4
lucapl <lukasz.andryszewski@outlook.com>
2023-12-29 14:10:39 +0100

    utility functions

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def visualize(history):
+    # summarize history for accuracy
+    plt.plot(history.history['binary_accuracy'])
+    plt.plot(history.history['val_binary_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+def plot_sensors(xl,yl,first=5):
+	for i in range(first):
+		plt.plot(xl[i])
+		print(yl[i])
+		plt.show()
